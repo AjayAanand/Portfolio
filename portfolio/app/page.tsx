6d3eb9809df39833519bdf8b0bef34fa98017b06
AjStar7 <ajay.anand@ecarasolutions.in>
2024-03-02 14:56:23 +0530

    ""

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
+    <div style={{flex:1,justifyContent:'center', borderWidth:2, borderColor:'red'  }}>
+     <div style={{justifyItems:'center'}}>
+     <h1 style={{fontSize:60, alignSelf:'center' ,justifyContent:'center'}}>
         Ajay Anand
       </h1>
+     </div>
     </div>
   );
 }
